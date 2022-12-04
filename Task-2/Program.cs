c61b5593a868824904bc664dfa87e29af2ede0e5
Maks <kpasowsky@gmail.com>
2022-12-04 21:13:09 +0300

    Task 2

---
+++ Task-2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+    Console.WriteLine("Кратно");
+else 
+    Console.WriteLine($"Не кратно, остаток {n % m}");
