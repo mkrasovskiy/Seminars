9954c090b48051c397d0e6203360a4a227fda998
Maks <kpasowsky@gmail.com>
2022-12-04 21:01:03 +0300

    Task 1

---
+++ Task-1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерированное число {number}");
+int n1 = number / 100;
+int n3 = number % 10;
+Console.WriteLine(n1 * 10 + n3 * 1);
