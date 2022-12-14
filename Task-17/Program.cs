bda0dda2c75114fdeefaeca847f6ec042e5781a3
Maks <kpasowsky@gmail.com>
2022-12-15 00:28:17 +0300

    Task 17

---
+++ Task-17/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11);
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+        Console.Write($"{array[i] * array[array.Length -1 - i]} ");
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив [{string.Join(", ", array)}]");
+ReleaseArray(array);
