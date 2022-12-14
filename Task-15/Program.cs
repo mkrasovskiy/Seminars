b9ca2117b7de71d89d03eba901f6df6acf862b31
Maks <kpasowsky@gmail.com>
2022-12-14 23:20:51 +0300

    Task 15

---
+++ Task-15/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+string ReleaseArray(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (number == array[i])
+            return "yes";
+    }
+    return "no";
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив [{string.Join(", ", array)}]");
+Console.Write("Введите число: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ReleaseArray(array, k));
