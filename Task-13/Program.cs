7dce61ef619c44823f6cc5fb0d3b0e2df3eed7a2
Maks <kpasowsky@gmail.com>
2022-12-14 22:58:17 +0300

    Task 13

---
+++ Task-13/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+void ReleaseArray(int[] array)
+{
+    int sumPositive = 0, sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sumPositive += array[i];
+        else
+            sumNegative += array[i];
+    }
+    Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");
+    Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}");
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
