1da1e43ec3f5627b9d74ab33adfd24cfde619bba
Maks <kpasowsky@gmail.com>
2022-12-18 22:44:44 +0300

    Task 19

---
+++ Task-19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите стороны треугольника: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if ((n1 + n2 > n3) && (n1 + n3 > n2) && (n2 + n3 > n1))
+Console.Write("Может");
+else
+Console.Write("Не Может");
