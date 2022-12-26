183bb4ca3ef7a8ac86935c1509409c902a96887b
Maks <kpasowsky@gmail.com>
2022-12-26 22:07:22 +0300

    Task 28

---
+++ Task-28/Program.cs
@@ -0,0 +1,67 @@
+﻿bool CheckNumberInArray(int[] array, int number)
+{
+    foreach (int element in array)
+    {
+        if (element == number)
+            return false;
+    }
+    return true;
+}
+
+
+int InputMatrix(int[,] matrix, int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [-10, 10]
+            if (CheckNumberInArray(array, matrix[i, j]) == true) // можно использовать без "==  true"
+            {
+                array[count] = matrix[i, j];
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix, int[] array, int count)
+{
+    for (int k = 0; k < count; k++)
+    {
+        int countNumbers = 1;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = i + 1; j < matrix.GetLength(1); j++)
+            {
+                if (array[k] == matrix[i, j])
+                    countNumbers++;
+            }
+        }
+        Console.WriteLine($"{array[k]} встречается {countNumbers} раз");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int[] array = new int[size[0] * size[1]];
+Console.WriteLine("Начальный массив:");
+int count = InputMatrix(matrix, array);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix, array, count);
