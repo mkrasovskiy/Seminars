737baec79c9f94d2083511e2b79946c93e401d81
Maks <kpasowsky@gmail.com>
2022-12-30 19:52:04 +0300

    Task 33

---
+++ Task-33/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Pow(m, n)}");
+
+int Pow(int number, int rank)
+{
+    if(rank == 0) return 1;
+    return (number * Pow(number, rank - 1));
+}
