016c87de39f39a181dd85a9afb77efc88f6e51de
Maks <kpasowsky@gmail.com>
2022-12-30 19:45:21 +0300

    Task 32

---
+++ Task-32/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{m}->{SumNumbers(m)}");
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+        return 0;
+    return (number % 10 + SumNumbers(number / 10));
+}
