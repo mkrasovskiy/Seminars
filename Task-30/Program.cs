30440088aaffd18595f848e69684aed1fb127422
Maks <kpasowsky@gmail.com>
2022-12-30 19:32:11 +0300

    Task 30

---
+++ Task-30/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int end)
+{
+    if (end == 1)
+    {
+        Console.WriteLine(1);
+        return "1";
+    }
+string s = PrintNumbers(end - 1) + ' ' + end.ToString();
+Console.WriteLine(s);
+return (s);
+}
